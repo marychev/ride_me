413a7e8de65193fffc402e1c2ab9cffeda40e0f1
Mikhail <marychevmihail3737@gmail.com>
2020-10-14 21:18:14 +0300

    draft screen manager

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from conf import *      # <-- don't delete!
 from kivy.app import App
-from rideme_game import RideMeGame
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
+
+
+ride_me = Builder.load_file("ride_me.kv")
+
+
+class StartScreen(Screen):
+    pass
+
+
+class GameScreen(Screen):
+    pass
+
+
+sm = ScreenManager(transition=WipeTransition())
+sm.add_widget(StartScreen(name='start'))
+sm.add_widget(GameScreen(name='game'))
 
 
 class RideMeApp(App):
     def build(self):
-        return RideMeGame()
+        return sm
 
 
 if __name__ == '__main__':
