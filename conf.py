118ec24e8501397890eb3b0232a9cbb6d94f7dea
marychev <marychevmihail3737@gmail.com>
2021-04-05 19:40:02 +0300

    add utils about sizes of different phones

--- conf.py
+++ conf.py
@@ -1,13 +1,20 @@
 import kivy
-#kivy.require('2.0.0')
+kivy.require('2.0.0')
 from kivy.config import Config
 from kivy.utils import platform
 from kivy.core.window import Window
+from utils.size_phones import SizePhones
 
 SECOND_GAME = 1.0 / 60.0
-HEIGHT_GAME = 560 + 100
-WIDTH_GAME = 860 + 200
+
+sp = SizePhones()
+HEIGHT_GAME = int(sp.height/2)
+WIDTH_GAME = int(sp.width/2)
+
 
 if platform not in ('android', 'ios'):
     Config.set('graphics', 'resizable', '1')
     Window.size = (WIDTH_GAME, HEIGHT_GAME)
+
+
+
